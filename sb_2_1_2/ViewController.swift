d54c0ff74810521ba351099811c152a2ef1b340e
Artem Shalin <a.shalin@rtinform.ru>
2021-12-28 23:36:59 +0300

    2.1.2

--- sb_2_1_2/ViewController.swift
+++ sb_2_1_2/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redUIView: UIView!
+    @IBOutlet var yellowUIView: UIView!
+    @IBOutlet var greenUIView: UIView!
+    
+    @IBOutlet var startButtonTitle: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redUIView.layer.cornerRadius = 100
+        yellowUIView.layer.cornerRadius = 100
+        greenUIView.layer.cornerRadius = 100
+        
     }
 
-
+    @IBAction func startButtonAction(_ sender: Any) {
+        
+        if redUIView.alpha == 1 {
+            redUIView.alpha = 0.3
+            yellowUIView.alpha = 1
+        } else if yellowUIView.alpha == 1 {
+            yellowUIView.alpha = 0.3
+            greenUIView.alpha = 1
+        } else if greenUIView.alpha == 1 {
+            greenUIView.alpha = 0.3
+            redUIView.alpha = 1
+        } else {
+            startButtonTitle.setTitle("NEXT", for: .normal)
+            redUIView.alpha = 1
+        }
+    }
 }
 
